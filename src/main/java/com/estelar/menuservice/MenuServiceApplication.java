b09f0dc3cb4db99b55dcaceb64d8d834121b7495
George Sandoval <george.sandoval.estela@gmail.com>
2024-01-03 00:13:34 -0500

    initial commit

---
+++ src/main/java/com/estelar/menuservice/MenuServiceApplication.java
@@ -0,0 +1,13 @@
+package com.estelar.menuservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MenuServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MenuServiceApplication.class, args);
+	}
+
+}
