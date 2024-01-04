61e8bf3ebd4117874cc83c901cb68ab9159620c5
George Sandoval <george.sandoval.estela@gmail.com>
2024-01-04 00:37:33 -0500

    add repostory

---
+++ src/main/java/com/estelar/menuservice/repository/FoodRepository.java
@@ -0,0 +1,9 @@
+package com.estelar.menuservice.repository;
+
+import com.estelar.menuservice.domain.Food;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FoodRepository extends JpaRepository<Food, Long> {
+}
